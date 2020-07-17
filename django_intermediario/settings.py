cca8ea4e64b7c123a4b7819c7f1ad282e22486d2
Victor HugoJava <54379876+VICTORHUGOJS@users.noreply.github.com>
2020-07-17 15:04:45 -0300

    autentic login

--- django_intermediario/settings.py
+++ django_intermediario/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_intermediario.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'django_intermediario.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'django'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', '88658710'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect #Redirect page
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as django_login
 
 # Create your views here.
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'my_app/login.html')
+    
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    user = authenticate(username=username,password=password) #Autentic
+    
+    if user:
+        django_login(request,user)
+        #return HttpResponseRedirect('/home/') #Redirect pageHome
+        return redirect('/home/')
+    message = 'Credencias inválida'
+    return render(request, 'my_app/login.html', {'message': message})
+  
+        
+        
+def home(request):
+    return render(request, 'my_app/home.html')
